f51ba69a7b1dab29490bb431eec4e04b27999878
chromeheartslens <sabequeoscriaesauce@gmail.com>
2025-06-30 20:16:01 -0300

    aaa

--- MainFunctions.lua
+++ MainFunctions.lua
@@ -1,1 +1,32 @@
-return true
+local Mainfuncs = {}
+
+local RevertData = {}
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+local TweenService = game:GetService("TweenService")
+local HttpService = game:GetService("HttpService")
+local MessagingService = game:GetService("MessagingService")
+local TeleportService = game:GetService("TeleportService")
+local DataStoreService = game:GetService("DataStoreService")
+local TextService = game:GetService("TextService")
+
+local DataManager = require(ServerStorage.Modules.Managers.DataManager)
+local StatData = require(ServerStorage.Modules.Utility.StatData)
+
+function Mainfuncs:PlayerAdded(Player)
+    local s, e = pcall(function()
+    
+    end)
+    if not s then end;
+end
+
+function Mainfuncs:PlayerRemoving(Player)
+    local s, e = pcall(function()
+    
+    end)
+    if not s then end;
+end
+
+return Mainfuncs
